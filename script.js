141e4908ca2e864d853c2e7b0be6e2d3e7d8c9f7
ajaykumar8288 <ajay251002@gmail.com>
2024-01-18 13:24:24 +0530

    Variable and var keyword in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+// Different type of write message in JavaScript
+/*
 alert("Welcome to AjTechAjay")
 document.write("Welcome to AjTechAjay")
 prompt("Hello World")
+console.log("Hello world")*/
+
+/* Variable in JavaScript
+  |=> Variable name must be with alphabet, $(dollor) and _(underscore).
+  |=> Variable has 3 types : 1. var, 2. let, 3. const
+  */
+
+  //Var keyword
+var a = 10
+var $a = 20
+var _name = "AjTech"
+console.log(a)
+console.log($a)
+console.log(_name)
+
+
